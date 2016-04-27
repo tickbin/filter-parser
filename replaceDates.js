0e4cbf0ca20c2eb5ae9234fc0307ee48d5f1cd1d
Jonathan Bowers <jonotron@gmail.com>
2016-04-27 10:54:00 -0700

    replace dates

---
+++ replaceDates.js
@@ -0,0 +1,20 @@
+import chrono from 'chrono-node'
+import wholeMonth from 'chrono-refiner-wholemonth'
+
+export default replaceDates
+
+const parser = new chrono.Chrono
+parser.refiners.push(wholeMonth)
+
+function replaceDates(str) {
+  let result = str
+
+  for (let match of parser.parse(str)) {
+    let start = match.start.date().toISOString()
+    let end = match.end.date().toISOString()
+    let dateText = `dateBetween(date, "${start}", "${end}")`
+    result = result.replace(match.text, dateText)
+  }
+
+  return result
+}
---
+++ test/replaceDates.test.js
@@ -0,0 +1,8 @@
+import test from 'tape'
+import replaceDates from '../replaceDates'
+
+test('replace single date', t => {
+  const str = replaceDates('Feb - Mar')
+  t.equals(str, 'dateBetween(date, "2016-02-01T08:00:00.000Z", "2016-04-01T06:59:59.999Z")')
+  t.end()
+})
