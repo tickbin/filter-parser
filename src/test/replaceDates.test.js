91eaef1647f378b60e81d80b546ef6b81b0cc3d2
Jonathan Bowers <jonotron@gmail.com>
2016-04-28 11:07:19 -0700

    Release compiled (#5)
    * restructure to release compiled code
    
    * fix: build should run on src/
    
    * test under UTC timezone

--- src/test/replaceDates.test.js
+++ src/test/replaceDates.test.js
@@ -3,6 +3,6 @@ import replaceDates from '../replaceDates'
 
 test('replace single date', t => {
   const str = replaceDates('Feb - Mar')
-  t.equals(str, 'dateBetween(start, "2016-02-01T08:00:00.000Z", "2016-04-01T06:59:59.999Z")')
+  t.equals(str, 'dateBetween(start, "2016-02-01T00:00:00.000Z", "2016-03-31T23:59:59.999Z")')
   t.end()
 })
