2814528d7e5609b67787dac0b3c6ccc3a0904e98
Jonathan Bowers <jonotron@gmail.com>
2016-05-06 12:41:54 -0700

    ensure order of operations

--- src/test/replaceDates.test.js
+++ src/test/replaceDates.test.js
@@ -3,6 +3,6 @@ import parse from '../'
 
 test('replace single date', t => {
   const str = parse('Feb - Mar')
-  t.equals(str, 'startArr >= [2016,1,1,0,0,0,0] and startArr <= [2016,2,31,23,59,59,999]')
+  t.equals(str, '(startArr >= [2016,1,1,0,0,0,0] and startArr <= [2016,2,31,23,59,59,999])')
   t.end()
 })
