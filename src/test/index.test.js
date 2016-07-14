bc791e0b81965938e4a1d77a3a5528e90995f7ef
Chad Fawcett <me@chadf.ca>
2016-07-14 16:26:12 -0700

    Explain test a bit more

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -25,6 +25,7 @@ test('date relative to timezone offset', t => {
   //  Timezone of -24 hours (-1440 minites) to ensure different date
   const {dates} = parse('Feb - Mar 2016', -1440)
 
+  //  Feb 1 12am - Mar 31 11:59pm in UTC-1440 is the same as Feb 2 12am - April 1 11:59pm in UTC
   t.equals(dates[0].text, '(startArr >= [2016,1,2,0,0,0,0] and startArr <= [2016,3,1,23,59,59,999])')
   t.end()
 })
